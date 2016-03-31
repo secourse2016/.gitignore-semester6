1503a4861267ce44544949d19ee9862561e14bac
2shraaf <ahmed.awwad.me@gmail.com>
2016-03-31 20:04:51 +0200

    add ng-s and in tags and handle newsletter

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('austrianAirlinesApp', []);
+/**
+ * take the mail from the view and make
+ * post request to add the mail to Newsletter
+ * @uncomment after creat the Post rout
+ */
+app.controller('masterController', function($scope) {
+    // $scope.subscrib = function() {
+    //   $http.post("api/subscrib",$scope.subscriberMail)
+    //   .success(function(data) {
+    //           $scope.subscriberMail = {};
+    //   })
+    //   .error(function(data) {
+    //           console.log('Error: ' + data);
+    //   });
+    // };
+});
