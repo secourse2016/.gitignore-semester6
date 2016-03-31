80382f9a88dffd220bdb310ad2b32edf92561316
Omar Radwan <ommarradwan213@gmail.com>
2016-03-31 18:40:21 +0200

    created a new js file to adjust the date picker using jQuery

---
+++ public/js/date-picker.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+	$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
+});
+          
