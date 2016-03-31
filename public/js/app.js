40a2788d339c20c15ee2952768264139580e4433
2shraaf <ahmed.awwad.me@gmail.com>
2016-03-31 21:44:51 +0200

    add routs

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,64 @@
-var app = angular.module('austrianAirlinesApp', []);
+var app = angular.module('austrianAirlinesApp', ['ngRoute']);
+/**
+ * configure master page routes
+ * @controller need to be added
+ */
+app.config(function($routeProvider , $locationProvider) {
+    $routeProvider.when('/', {
+            templateUrl : 'views/landing.html'
+        })
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'views/about.html'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'views/contact.html'
+        })
+        // route for the help page
+        .when('/help', {
+            templateUrl : 'views/help.html'
+        })
+        // route for the Terms & Conditions page
+        .when('/termsAndConditions', {
+            templateUrl : 'views/termsAndConditions.html'
+        })
+        // route for the Privacy Policy page
+        .when('/privacypolicy', {
+            templateUrl : 'views/privacypolicy.html'
+        })
+        // route for the Booking a Flight page
+        .when('/bookAFlight', {
+            templateUrl : 'views/bookAFlight.html'
+        })
+        // route for the Offers page
+        .when('/offers', {
+            templateUrl : 'views/offers.html'
+        })
+        // route for the Pricing page
+        .when('/pricing', {
+            templateUrl : 'views/pricing.html'
+        });
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+
+});
 /**
  * take the mail from the view and make
  * post request to add the mail to Newsletter
  * @uncomment after creat the Post rout
  */
 app.controller('masterController', function($scope) {
-    // $scope.subscrib = function() {
-    //   $http.post("api/subscrib",$scope.subscriberMail)
-    //   .success(function(data) {
-    //           $scope.subscriberMail = {};
-    //   })
-    //   .error(function(data) {
-    //           console.log('Error: ' + data);
-    //   });
-    // };
+    $scope.subscrib = function() {
+      // $http.post("api/subscrib",$scope.subscriberMail)
+      // .success(function() {
+      //         $scope.subscriberMail = {};
+      //         console.log('Done: ' + data);
+      //
+      // })
+      // .error(function() {
+      //         console.log('Error: ' + data);
+      // });
+    };
 });
