b6a56a135914d9952c82e7fd6a6e4a7c58b8ec8b
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2016-03-31 15:23:26 +0200

    Added package.json and server file

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var path    = require('path');
+var app = express();
+app.set('views', path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/",function(req,res){
+	res.render("index.html");
+});
+
+app.listen(80);
