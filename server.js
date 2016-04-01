58fceab5bd1884be9db274d8a77a772d403f613a
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2016-04-01 18:39:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,40 @@ var path    = require('path');
 var morgan = require('morgan');
 
 var app = express();
-
-app.use(morgan('dev'));
 app.set('views', path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",function(req,res){
 	res.render("index.html");
 });
-app.route('/about').get(function(req, res) {
-    return res.sendFile(__dirname + '/public/index.html');
+app.route('/about').get(function(req, res) { 
+    return res.sendFile(__dirname + '/public/index.html'); 
 });
 
-app.route('/help').get(function(req, res) {
-    return res.sendFile(__dirname + '/public/index.html');
+app.route('/help').get(function(req, res) { 
+    return res.sendFile(__dirname + '/public/index.html'); 
 });
 
-app.route('/contact	').get(function(req, res) {
-    return res.sendFile(__dirname + '/public/index.html');
+app.route('/contact').get(function(req, res) { 
+    return res.sendFile(__dirname + '/public/index.html'); 
 });
 
-app.listen(8080);
-// app.listen(80);
+app.route('/termsAndConditions').get(function(req, res) { 
+    return res.sendFile(__dirname + '/public/index.html'); 
+});
+
+app.route('/privacypolicy').get(function(req, res) { 
+    return res.sendFile(__dirname + '/public/index.html'); 
+});
+
+app.route('/bookAFlight').get(function(req, res) { 
+    return res.sendFile(__dirname + '/public/index.html'); 
+});
+
+app.route('/offers').get(function(req, res) { 
+    return res.sendFile(__dirname + '/public/index.html'); 
+});
+
+app.route('/pricing').get(function(req, res) { 
+    return res.sendFile(__dirname + '/public/index.html'); 
+});
+app.listen(8000);
