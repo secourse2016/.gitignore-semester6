40720dd57c73dfa77764a92a98ed0e2818a12911
AhmedSoliman7 <AhmedSoliman7@github.com>
2016-04-01 14:10:50 +0200

    added passengerViewCtrl / added passengers.html passenger-info Form

--- public/js/controllers/passengerViewCtrl.js
+++ public/js/controllers/passengerViewCtrl.js
@@ -1,6 +1,7 @@
 var passengerViewApp = angular.module('passengerViewApp' , []);
-
-
 passengerViewApp.controller('passengerViewCtrl' , function($scope){
-  
+  //for testing.
+  $scope.passengers = [   {"type" : 'Adult'}  , {"type" : 'Child'} ];
+    // to be filled.
+    $scope.formData =[];
 });
