5d544705eb351ecaba9421990500ddfa18e7b1ce
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-04-01 17:46:05 +0200

    added morgan to show log requests

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 var express = require("express");
 var path    = require('path');
+var morgan = require('morgan');
+
 var app = express();
+
+app.use(morgan('dev'));
 app.set('views', path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",function(req,res){
 	res.render("index.html");
 });
-app.route('/about').get(function(req, res) { 
-    return res.sendFile(__dirname + '/public/index.html'); 
+app.route('/about').get(function(req, res) {
+    return res.sendFile(__dirname + '/public/index.html');
 });
 
-app.route('/help').get(function(req, res) { 
-    return res.sendFile(__dirname + '/public/index.html'); 
+app.route('/help').get(function(req, res) {
+    return res.sendFile(__dirname + '/public/index.html');
 });
 
-app.route('/contact	').get(function(req, res) { 
-    return res.sendFile(__dirname + '/public/index.html'); 
+app.route('/contact	').get(function(req, res) {
+    return res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(8080);
