dac1b91d95dedfac5b3a4cabcd5e94f8790aff48
Omar Radwan <ommarradwan213@gmail.com>
2016-04-01 18:08:38 +0200

    trying to fix date picker

--- public/js/date-picker.js
+++ public/js/date-picker.js
@@ -1,9 +1,15 @@
 
 $(document).ready(function(){
+
 	$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    	selectMonths: true, // Creates a dropdown to control month
+    	selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 
+	console.log("3ww");
+	$('#button').on('click' , function(){
+		console.log("3w");
+		$('#form').hide();
+	});
 });
           
