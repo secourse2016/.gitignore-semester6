17481916da05da2838a5ce02fd362518837b83fb
Omar Radwan <ommarradwan213@gmail.com>
2016-04-03 11:16:40 +0200

    save the form inputs and merge it with the coming booking data

--- public/js/controllers/paymentController.js
+++ public/js/controllers/paymentController.js
@@ -2,6 +2,21 @@
 (function(){
 	angular.module('austrianAirlinesApp')
 	.controller('PaymentController', function(){
-	this.totalPrice = 3000;
-});
+		this.booking = {}; // get all booking info from the global service
+		this.totalPrice = 3000; // = booking.totalPrice
+		
+		// Payment Form attributes
+		this.cardNo = null;
+		this.cvv = null;
+		this.expiryDate = null;
+
+		// Add the form inputs to the booking array
+		this.addBooking = function(){
+			console.log("Hello");
+			this.booking.cardNo = this.cardNo;
+			this.booking.cvv = this.cvv;
+			this.booking.expiryDate = this.expiryDate;
+			// insert booking into the dataBase
+		};
+	});
 })();
