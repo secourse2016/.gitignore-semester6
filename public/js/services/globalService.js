5003bbcd38ea921dbadd3d87707ca7b4344787c5
AhmedSoliman7 <ahmed.soliman9595@gmail.com>
2016-04-04 15:15:00 +0300

    Update globalService.js
    Resolved conflicts

--- public/js/services/globalService.js
+++ public/js/services/globalService.js
@@ -12,11 +12,7 @@ angular.module('austrianAirlinesApp').service('global', function() {
 	this.searchFlight = {};
 
 	// A list of passengers whose info are entered by the user
-<<<<<<< HEAD
-=======
-	
-	
->>>>>>> 3373d24836c147bc115ffa1b5cea283e58a21c18
+
 
 	this.passengers = [];
 
