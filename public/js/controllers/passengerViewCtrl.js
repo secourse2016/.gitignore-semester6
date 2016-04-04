239c1028b835a4f770a2fbafae103a9cec9b6291
AhmedSoliman7 <ahmed.soliman9595@gmail.com>
2016-04-04 17:43:14 +0200

    User can enter details of all passengers according to number in search form

--- public/js/controllers/passengerViewCtrl.js
+++ public/js/controllers/passengerViewCtrl.js
@@ -2,17 +2,18 @@ var app = angular.module('austrianAirlinesApp');
 app.controller('passengerViewCtrl' , function($scope, global, $location){
 
   //for testing.
-  $scope.passengers = [{type : 'Adult'},{type:'Child'} ];
+  // $scope.passengers = [{type : 'Adult'},{type:'Child'} ];
   //for filling it with passengers info .
   $scope.formData =[];
 
     // Get all information from the global service, to be used in the view
 
       $scope.infoFlow = global;
-  
-      $scope.adults = new Array(global.searchFlight.adults);
-      $scope.children = new Array(global.searchFlight.children);
-      
+
+      $scope.adults = new Array(parseInt(global.searchFlight.adults));
+      $scope.children = new Array(parseInt(global.searchFlight.children));
+      console.log($scope.adults);
+      console.log($scope.children);
       $scope.submitForm = function() {
       // set passengers Array in service with filled passengers info .
       global.setPassengers($scope.formData);
