b76fdeb064bedb2b437f9ca089ae31241b60260d
2shraaf <ahmed.awwad.me@gmail.com>
2016-04-13 19:36:27 +0200

    #50 Models done date may change

--- app/models/airport.js
+++ app/models/airport.js
@@ -2,15 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var airportSchema = new Schema({
-  iata:    { type: String, maxlength: 3,uppercase: true },
-  lon:  Number,
-  iso:  { type: String, maxlength: 2,uppercase: true },
-  status:  Number,
-  name:  String,
-  continent:  { type: String, maxlength: 2,uppercase: true },
-  type:  String,
-  lat:  Number,
-  size:  String
+  iata         :    { type: String, maxlength: 3,uppercase: true },
+  lon          :  Number,
+  iso          :  { type: String, maxlength: 2,uppercase: true },
+  status       :  Number,
+  name         :  String,
+  continent    :  { type: String, maxlength: 2,uppercase: true },
+  type         :  String,
+  lat          :  Number,
+  size         :  String
 });
 
 
--- app/models/flight.js
+++ app/models/flight.js
@@ -2,17 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var flightSchema = new Schema({
-  flightNumber:  String,
-  aircraftType:  { type: String ,default: 'Boeing'},
-  aircraftModel:  { type: Number ,default: 747},
-  departureDateTime:  Date,
-  arrivalDateTime:  Date,
-  origin:  { type: String, maxlength: 3,uppercase: true },
-  destination:  { type: String, maxlength: 3,uppercase: true },
-  cost:  Number,
-  currency:  { type: String, maxlength: 3,uppercase: true ,default: 'USD'},
-  class:  {type: String, enum: ['economy','business'], default: 'economy'},
-  Airline:  {type: String, default: 'Austrian'}
+  flightNumber      :  String,
+  aircraftType      :  { type: String ,default: 'Boeing'},
+  aircraftModel     :  { type: Number ,default: 747},
+  departureDateTime :  Date,
+  arrivalDateTime   :  Date,
+  origin            :  { type: String, maxlength: 3,uppercase: true ,ref: 'Airport' },
+  destination       :  { type: String, maxlength: 3,uppercase: true ,ref: 'Airport' },
+  cost              :  Number,
+  currency          :  { type: String, maxlength: 3,uppercase: true ,default: 'USD' },
+  class             :  {type: String, enum: ['economy','business'], default: 'economy' },
+  Airline           :  {type: String, default: 'Austrian'}
 });
 
 
