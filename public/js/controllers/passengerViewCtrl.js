d44cca2565918d4730e1c1a9f7091395c40528cd
AhmedSoliman7 <ahmed.soliman9595@gmail.com>
2016-04-14 14:20:00 +0200

    #57 added loop over data submitted to check validity using error counting (not completed yet)

--- public/js/controllers/passengerViewCtrl.js
+++ public/js/controllers/passengerViewCtrl.js
@@ -5,7 +5,7 @@ app.controller('passengerViewCtrl' , function($scope, global, $location){
   // $scope.passengers = [{type : 'Adult'},{type:'Child'} ];
   //for filling it with passengers info .
   $scope.formData =[];
-
+  $scope.errors =[];
     // Get all information from the global service, to be used in the view
       if(!global.searchFlight.adults)
         global.searchFlight.adults = 1;
@@ -18,11 +18,38 @@ app.controller('passengerViewCtrl' , function($scope, global, $location){
       $scope.step = 2;
 
       $scope.submitForm = function() {
+
+      // boolean value to check the validation of the form.
+        var isValid = true;
+
+      // integer to indicate errors occurences
+        var countErrors = 0;
+
+      // loop in formData submitted to check fields.
+        for (var i = 0; i < $scope.formData.length; i++) {
+          if(!$scope.formData[i].firstName) {
+            if(!$scope.errors[i])
+              $scope.errors[i] = {};
+            $scope.errors[i].firstName = true;
+            countErrors++;
+          }
+          if(!$scope.formData[i].lastName) {
+            if(!$scope.errors[i])
+              $scope.errors[i] = {};
+            $scope.errors[i].lastName = true;
+            countErrors++;
+          }
+        }
+        // if error(s) found isValid is set to false.
+        if(countErrors > 0 )
+        isValid = false;
+
+      // check validity of the form .
+        if(isValid) {
       // set passengers Array in service with filled passengers info .
         global.setPassengers($scope.formData);
+      // redirect to confirmation page.
         $location.path('/confirmation');
-
-      // for testing (Working)
-      // console.log(global.getPassengers());
+      }
     }
 });
