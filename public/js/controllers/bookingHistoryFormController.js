5e3501201f4cfcde1d0abce6ee415b5ddd8e91ca
omarradwan <omarradwan213@gmail.com>
2016-04-14 16:56:48 +0200

    #56 pass the booking data to the service

--- public/js/controllers/bookingHistoryFormController.js
+++ public/js/controllers/bookingHistoryFormController.js
@@ -1,13 +1,16 @@
 (function(){
 	angular.module('austrianAirlinesApp')
-	.controller('historyFormController', function($scope){
+	.controller('historyFormController', ['$http' , function($http , bookingHistoryService){
 		// Booking history form attributes
-		this.bookingNo = null;
-		this.passportNo = null;
-
+		this.request = null;
+		this.booking = null;
+		var Ctrl = this;
     // Search the booking in the database
 		this.searchBooking = function(){
-
+			$http.post('/api/search-booking' , this.request).success(function(data){
+				Ctrl.booking = data;
+				bookingHistoryService.setBooking(data);
+			});
 		};
-	});
+	}]);
 })();
