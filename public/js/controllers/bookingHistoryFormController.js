53e7c406b51a428d9c3032a0f0c03058eddde8e1
omarradwan <omarradwan213@gmail.com>
2016-04-14 11:00:21 +0200

    create history form comtroller

---
+++ public/js/controllers/bookingHistoryFormController.js
@@ -0,0 +1,13 @@
+(function(){
+	angular.module('austrianAirlinesApp')
+	.controller('bookingHistoryFormController', function($scope){
+		// Booking history form attributes
+		this.bookingNo = null;
+		this.passportNo = null;
+
+    // Search the booking in the database
+		this.search = function(){
+      
+		};
+	});
+})();
