3f03175c3d88adc16c3ff8b1f73b719e29104f00
2shraaf <ahmed.awwad.me@gmail.com>
2016-04-14 14:23:13 +0200

    #50 delete unwanted files

--- database/seed.js
+++ database/seed.js
@@ -1,15 +1,15 @@
 var airports           = require("../app/data/airports.json");
 var flights            = require("../app/data/flights.json");
-var Flight             = require('../app/models/flight');
-var Airport            = require('../app/models/airport');
+var flight             = require('../app/models/flight');
+var firport            = require('../app/models/airport');
 
 /**
  * seed airport from airpors.json file if collection is empty
  */
 exports.seedAirports = function seedAirports(cb){
-    Airport.collection.count( function ( err , c) { //check thier count
+    airport.collection.count( function ( err , c) { //check thier count
       if(c == 0)
-        Airport.create(airports, function(err, result) {cb(err,true)});
+        airport.create(airports, function(err, result) {cb(err,true)});
       else
           cb(err, false);
     });
@@ -19,9 +19,9 @@ exports.seedAirports = function seedAirports(cb){
  * seed flight from data in flights.json if collection is empty
  */
 exports.seedFlights = function seedFlights(cb){
-    Flight.collection.count( function (err , c) { //check thier count
+    flight.collection.count( function (err , c) { //check thier count
       if(c == 0)
-        Flight.create(flights, function(err, result) {cb(err,true)});
+        flight.create(flights, function(err, result) {cb(err,true)});
       else
           cb(err, false);
     });
