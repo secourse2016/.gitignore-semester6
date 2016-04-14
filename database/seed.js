14f453b8d93b786be0ca290cf0d09e168432a0eb
2shraaf <ahmed.awwad.me@gmail.com>
2016-04-14 12:31:33 +0200

    #50 seed flights

--- database/seed.js
+++ database/seed.js
@@ -1,11 +1,27 @@
-var Airports           = require("./airports.json")
+var airports           = require("./airports.json");
+var flights            = require("./flights.json");
 var Flight             = require('../app/models/flight');
 var Airport            = require('../app/models/airport');
 
+/**
+ * seed airport from airpors.json file if collection is empty
+ */
 exports.seedAirports = function seedAirports(cb){
     Airport.collection.count( function ( err , c) { //check thier count
       if(c == 0)
-        Airport.collection.insert(Airports, function(err, result) {cb(err,true)});
+        Airport.create(airports, function(err, result) {cb(err,true)});
+      else
+          cb(err, false);
+    });
+}
+/**
+ * seed flight from data in flights.json if collection is empty
+ */
+
+exports.seedFlights = function seedFlights(cb){
+    Flight.collection.count( function (err , c) { //check thier count
+      if(c == 0)
+        Flight.create(flights, function(err, result) {cb(err,true)});
       else
           cb(err, false);
     });
