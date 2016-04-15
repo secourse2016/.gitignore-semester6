8f705f267910393ff39ccbe445fe79ba5000ccb0
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2016-04-15 17:33:32 +0200

    #54 | a POST request is sent to the API when the user searches for flights

--- public/js/controllers/landingController.js
+++ public/js/controllers/landingController.js
@@ -31,7 +31,7 @@
 	 * Search Flight Controller form collecting form data
 	 * and redirecting to the flights view.
 	 */
-	app.controller('searchFlightController', function($scope, global, $location){
+	app.controller('searchFlightController', function($scope, global, $location, $http){
 		this.range = [];
 		for(var i = 2; i <= 7; ++i)
 			this.range.push(i);
@@ -42,7 +42,41 @@
 			if($scope.formData.destination)
 				$scope.formData.destination = $scope.formData.destination.iata;
 			global.setSearchFlight($scope.formData);
-			$location.path("/flights");
+
+			// send a post request to get the flights
+			var origin 			= $scope.formData.origin;
+			var destination 	= $scope.formData.destination;
+			var outgoingDate	= $scope.formData.outgoingDate;
+			var returnDate		= $scope.formData.returnDate;
+			var tripType		= $scope.formData.tripType;
+			var allAirlines		= $scope.formData.allAirlines || false;
+			var flightClass		= $scope.formData.flightClass;
+
+			var requestParameters = {
+				'origin' 		: origin,
+				'destination'	: destination,
+				'departureDate'	: outgoingDate,
+				'flightClass'	: flightClass,
+				'allAirlines'	: allAirlines,
+			}
+
+			var postURL = 'api/flights/search/oneway';
+			if(tripType == 2 && returnDate) {
+				requestParameters.arrivalDate = returnDate;
+				postURL = 'api/flights/search/roundtrip';
+			}
+			console.log(requestParameters);
+			$http.post(postURL, requestParameters)
+			 .success(function(data){
+			 	// $location.path("/flights");
+				 console.log(data);
+			 })
+			 .error(function(data){
+            	 console.log('Error: can\'t fetch airports');
+        	});
+
+
+			
 		};
 	});
 
