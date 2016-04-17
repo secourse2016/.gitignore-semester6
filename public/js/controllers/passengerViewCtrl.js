f4b80ebf6189d0041a586365bcb268aff15aca0a
AhmedSoliman7 <ahmed.soliman9595@gmail.com>
2016-04-17 11:52:47 +0200

    #57 added validateAdults function to validate adults input fields

--- public/js/controllers/passengerViewCtrl.js
+++ public/js/controllers/passengerViewCtrl.js
@@ -5,118 +5,33 @@ app.controller('passengerViewCtrl' , function($scope, global, $location){
   // $scope.passengers = [{type : 'Adult'},{type:'Child'} ];
   //for filling it with passengers info .
   $scope.formData =[];
-  $scope.errors =[];
-
+  $scope.errors = [];
     // Get all information from the global service, to be used in the view
       if(!global.searchFlight.adults)
         global.searchFlight.adults = 1;
       if(!global.searchFlight.children)
         global.searchFlight.children = 0;
       $scope.infoFlow = global;
-
       $scope.adults = new Array(parseInt(global.searchFlight.adults));
       $scope.children = new Array(parseInt(global.searchFlight.children));
       $scope.step = 2;
-
       $scope.submitForm = function() {
-      // initialize errors array.
-        $scope.errors =[];
 
-      // boolean value to check the validation of the form.
-        var isValid = true;
-
-      // integer to indicate errors occurences
-        var countErrors = 0;
-
-      // loop in formData submitted to check fields.
-        for (var i = 0; i < $scope.formData.length; i++) {
-          // check if nationality field is empty.
-          if(!$scope.formData[i].firstName)
-          {
-            if(!$scope.errors[i])
-              $scope.errors[i] = {};
-            $scope.errors[i].firstName = true;
-            countErrors++;
-          }
-
-          // check if nationality field is empty.
-          if(!$scope.formData[i].lastName)
-          {
-            if(!$scope.errors[i])
-              $scope.errors[i] = {};
-            $scope.errors[i].lastName = true;
-            countErrors++;
-          }
-
-          // check if nationality field is empty.
-          if(!$scope.formData[i].nationality)
-          {
-            if(!$scope.errors[i])
-              $scope.errors[i] = {};
-            $scope.errors[i].nationality = true;
-            countErrors++;
-          }
-
-        // check if passport number field is empty or is not valid.
-        if(!$scope.formData[i].passportNumber)
-        {
-          if(!$scope.errors[i])
-            $scope.errors[i] = {};
-          $scope.errors[i].passportNumber = true;
-          countErrors++;
-        }
-        else if($scope.formData[i].passportNumber.length < 7)
-        {
-        if(!$scope.errors[i])
-          $scope.errors[i] = {};
-        $scope.errors[i].passportNumberLength = true;
-        countErrors++;
-        }
-
-        // check if email address field is empty or is not valid.
-        if(!validateEmail($scope.formData[i].emailAddress))
-        {
-        if(!$scope.errors[i])
-          $scope.errors[i] = {};
-          $scope.errors[i].emailAddressNotValid = true;
-          countErrors++;
-        }
-
-      // check if birthDate field is empty or is not valid.
-      if(!$scope.formData[i].birthDate) {
-        if(!$scope.errors[i])
-          $scope.errors[i] = {};
-        $scope.errors[i].birthDateRequired = true;
-        countErrors++;
-      }
-      else if(!validateDate($scope.formData[i].birthDate))
-      {
-        if(!$scope.errors[i])
-          $scope.errors[i] = {};
-        $scope.errors[i].birthDateNotValid = true;
-        countErrors++;
-      }
-
-        // if error(s) found isValid is set to false.
-        if(countErrors > 0 || $scope.formData.length == 0)
-        isValid = false;
-
-      // check validity of the form .
-        if(isValid) {
+      // function to validate adult form fields.
+        if(validateAdults($scope)) {
       // set passengers Array in service with filled passengers info .
         global.setPassengers($scope.formData);
       // redirect to confirmation page.
         $location.path('/confirmation');
       }
     }
-  }
-
 });
 
 /** function to validate birthDate of an adult.
  * returns boolean to indicate validity.
  */
-  function validateDate(stringDate) {
+  function validateDate(stringDate)
+  {
     var birthDate = new Date(Date.parse(stringDate));
     var nowDate = new Date(Date.now());
     return birthDate < nowDate && nowDate.getFullYear() - birthDate.getFullYear() >= 12;
@@ -127,9 +42,101 @@ app.controller('passengerViewCtrl' , function($scope, global, $location){
  * returns boolean to indicate validity.
  * Using regular expressions to test email format.
  */
-  function validateEmail(email) {
+  function validateEmail(email)
+  {
     if(!email)
     return false;
     var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
+  }
+
+/** function to validate adults form fields.
+ * takes scope and callback function as parameters.
+ * returns boolean isValid to indicate form fields validity.
+ */
+  function validateAdults($scope)
+  {
+    // initialize errors array.
+      $scope.errors =[];
+
+    // boolean value to check the validation of the form.
+      var isValid = true;
+
+    // integer to indicate errors occurences
+      var countErrors = 0;
+    // loop in formData submitted to check fields.
+      for (var i = 0; i < $scope.formData.length; i++) {
+        // check if nationality field is empty.
+        if(!$scope.formData[i].firstName)
+        {
+          if(!$scope.errors[i])
+            $scope.errors[i] = {};
+          $scope.errors[i].firstName = true;
+          countErrors++;
+        }
+
+        // check if nationality field is empty.
+        if(!$scope.formData[i].lastName)
+        {
+          if(!$scope.errors[i])
+            $scope.errors[i] = {};
+          $scope.errors[i].lastName = true;
+          countErrors++;
+        }
+
+        // check if nationality field is empty.
+        if(!$scope.formData[i].nationality)
+        {
+          if(!$scope.errors[i])
+            $scope.errors[i] = {};
+          $scope.errors[i].nationality = true;
+          countErrors++;
+        }
+
+      // check if passport number field is empty or is not valid.
+      if(!$scope.formData[i].passportNumber)
+      {
+        if(!$scope.errors[i])
+          $scope.errors[i] = {};
+        $scope.errors[i].passportNumber = true;
+        countErrors++;
+      }
+      else if($scope.formData[i].passportNumber.length < 7)
+      {
+      if(!$scope.errors[i])
+        $scope.errors[i] = {};
+      $scope.errors[i].passportNumberLength = true;
+      countErrors++;
+      }
+
+      // check if email address field is empty or is not valid.
+      if(!validateEmail($scope.formData[i].emailAddress))
+      {
+      if(!$scope.errors[i])
+        $scope.errors[i] = {};
+        $scope.errors[i].emailAddressNotValid = true;
+        countErrors++;
+      }
+
+    // check if birthDate field is empty or is not valid.
+    if(!$scope.formData[i].birthDate) {
+      if(!$scope.errors[i])
+        $scope.errors[i] = {};
+      $scope.errors[i].birthDateRequired = true;
+      countErrors++;
+    }
+    else if(!validateDate($scope.formData[i].birthDate))
+    {
+      if(!$scope.errors[i])
+        $scope.errors[i] = {};
+      $scope.errors[i].birthDateNotValid = true;
+      countErrors++;
+    }
+
+      // if error(s) found isValid is set to false.
+      if(countErrors > 0 || $scope.formData.length == 0)
+      isValid = false;
+
+      return isValid;
+  }
 }
