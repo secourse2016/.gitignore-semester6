ef0f776dd3907e16355d4e1f79c708178b8f6f8c
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-04-17 23:21:40 +0200

    enhanced styling and documentation of flight.js

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-
 /**
-* Require express main app
+* Require express main app.
 */
 var app = require('./app');
 
 /**
-* Require http
+* Require http.
 */
 var http = require('http');
 
 /**
-* Create a server
+* Create a server.
 */
 var server = http.createServer(app);
 
 /**
-* Get port number from .env file
+* Get port number from .env file.
 */
 var port = process.env.PORT;
 server.listen(port);
