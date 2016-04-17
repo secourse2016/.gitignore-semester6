889304eb73b6397701a9115297ab5aa32e134e4e
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-04-18 00:37:47 +0200

    enhanced identation of Stylesheets and database seed/clear

--- database/seed.js
+++ database/seed.js
@@ -4,43 +4,43 @@ var flight             = require('../app/models/flight');
 var airport            = require('../app/models/airport');
 
 /**
- * seed airport from airpors.json file if collection is empty
- */
+* seed airport from airpors.json file if collection is empty
+*/
 exports.seedAirports = function seedAirports(cb){
     airport.count(function(err, count){
-      if(count === 0)
-        airport.create(airports, function(err){
-             cb(err, true);
-         });
-      else
-          cb(err, false);
+        if(count === 0)
+            airport.create(airports, function(err){
+                cb(err, true);
+        });
+        else
+            cb(err, false);
     });
 }
 
 /**
- * seed flight from data in flights.json if collection is empty
- */
+* seed flight from data in flights.json if collection is empty
+*/
 exports.seedFlights = function seedFlights(cb){
     flight.count(function(err, count){
-      if(count === 0)
-        flight.create(flights, function(err){
-            cb(err, true);
+        if(count === 0)
+            flight.create(flights, function(err){
+                cb(err, true);
         });
-      else
-          cb(err, false);
+        else
+            cb(err, false);
     });
 }
 /**
- * seed both flights and airpors
- */
+* seed both flights and airpors
+*/
 exports.seed = function seed(cb){
     var seedFlights = this.seedFlights;
     this.seedAirports(function(err, check){
         if(err)
-            cb(err,check);
+        cb(err,check);
         else{
             seedFlights(function(err, check){
-              cb(err,check);
+                cb(err,check);
             });
         }
     });
