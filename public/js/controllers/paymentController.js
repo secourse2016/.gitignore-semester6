7fee763f54b343fdc0b7d10a4b88da68ee625369
omarradwan <omarradwan213@gmail.com>
2016-04-18 00:22:34 +0200

    #56 payment currency

--- public/js/controllers/paymentController.js
+++ public/js/controllers/paymentController.js
@@ -1,13 +1,13 @@
 (function(){
 	angular.module('austrianAirlinesApp')
 	.controller('PaymentController', function($scope , global){
-		this.booking = {}; // get all booking info from the global service
-		this.totalPrice = global.getTotalCost;
+		$scope.totalCost = global.getTotalCost();
+		$scope.currency = global.outGoingTrip.currency;
 		$scope.step = 4; // View number in the stepper
 		// Payment Form attributes
-		this.cardNo = null;
-		this.cvv = null;
-		this.expiryDate = null;
+		$scope.cardNo = 0;
+		$scope.cvv = 0;
+		$scope.expiryDate = {};
 
 		// Add the form inputs to the booking array
 		this.addBooking = function(){
@@ -18,6 +18,6 @@
 		};
 	})
 	.controller('successController' , function($scope , global){
-		$scope.bookingNumber = global.getBookingNumber;
+		$scope.bookingNumber = global.getBookingNumber();
 	});
 })();
