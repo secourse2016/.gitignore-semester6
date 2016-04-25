9340a071f66ba93264871c0ad9997a4e6faf460c
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2016-04-25 16:25:15 +0200

    #116 | Enhanced payment form validations

--- public/js/controllers/paymentController.js
+++ public/js/controllers/paymentController.js
@@ -10,45 +10,65 @@
 		*	Handle paying for the booking using stripe
 		*/
 		$scope.payForBooking = function(){
-			var expiryDate = new Date($scope.expiryDate);
+			$scope.error = {};
 
-			// Get the entered credit card information
-			var card = {
-				number: $scope.cardNo,
-				exp_month: (expiryDate.getMonth()+1),
-				exp_year: expiryDate.getFullYear(),
-				cvc : $scope.cvv
+			// Make sure that all fields are entered
+			var errored = false;
+			if(!$scope.cardNo) {
+				$scope.error.number = true;
+				errored = true;
 			}
-			
-			// Create the stripe token
-			Stripe.card.createToken(card, function(status,response){
-				
-				// Display the error message in the view in the appropriate place
-				if(response.error){
+			if(!$scope.cvv) {
+				$scope.error.cvv = true;
+				errored = true;
+			}
+			if(!$scope.expiryDate) {
+				$scope.error.date = true;
+				errored = true;
+			}
+			if(errored)
+				$scope.error.message = 'This field is required.';
+			else {
+				var expiryDate = new Date($scope.expiryDate);
 
-					$scope.error = {};
-					$scope.error.message = response.error.message;
-
-					if(response.error.param == 'number')
-						$scope.error.number = true;
-					else if(response.error.param == 'exp_month')
-						$scope.error.date = true;
-					else if(response.error.param == 'exp_year')
-						$scope.error.date = true;
-					else if(response.error.param == 'cvc')
-						$scope.error.cvv = true;
-
-					$scope.$apply();	
-				
+				// Get the entered credit card information
+				var card = {
+					number: $scope.cardNo,
+					exp_month: (expiryDate.getMonth()+1),
+					exp_year: expiryDate.getFullYear(),
+					cvc : $scope.cvv
 				}
-				else {
-					// TODO will send the booking details to the server here.
+				
+				// Create the stripe token
+				Stripe.card.createToken(card, function(status,response){
+					
+					// Display the error message in the view in the appropriate place
+					if(response.error){
+
+						
+						$scope.error.message = response.error.message;
+
+						if(response.error.param == 'number')
+							$scope.error.number = true;
+						else if(response.error.param == 'exp_month')
+							$scope.error.date = true;
+						else if(response.error.param == 'exp_year')
+							$scope.error.date = true;
+						else if(response.error.param == 'cvc')
+							$scope.error.cvv = true;
+
+						$scope.$apply();	
+					
+					}
+					else {
+						// TODO will send the booking details to the server here.
 
 
-					$location.path('/successful');
-				}
+						$location.path('/successful');
+					}
 
-			});
+				});
+			}
 			
 		};
 	})
