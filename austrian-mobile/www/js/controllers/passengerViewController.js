c0318aec5e28787ff54690ce4ee59c8da974a4ae
AhmedSoliman7 <ahmed.soliman9595@gmail.com>
2016-04-27 22:59:13 +0200

    #122 added files

---
+++ austrian-mobile/www/js/controllers/passengerViewController.js
@@ -0,0 +1,25 @@
+var app = angular.module('starter');
+
+app.controller('passengerViewController' , function($scope, global) {
+
+  $scope.departureDate    = "20th, April 2016";
+	$scope.arrivalDate      = "21th, April 2016";
+	$scope.origin           = "CAI";
+	$scope.destination      = "JED";
+	$scope.flightClass      = "Economy";
+	$scope.tripType         = 2;
+	$scope.outgoingFlight   = angular.copy(global.outGoingTrip);
+  if($scope.tripType == 2)
+		$scope.returnFlight = angular.copy(global.returnTrip);
+
+    $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    return $scope.shownGroup === group;
+  };
+});
