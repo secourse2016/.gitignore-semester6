93f3a96fedc530edd69c755cf6c62eb5740a820a
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-04-28 10:08:32 +0200

    added ionic material and ion-md-input

--- austrian-mobile/www/js/app.js
+++ austrian-mobile/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ionMdInput'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,7 +26,7 @@ angular.module('starter', ['ionic'])
   .state('index', {
     url: '/',
     views : {
-      
+
       'main-view' : {
         templateUrl: 'partials/home.html'
       },
@@ -34,7 +34,7 @@ angular.module('starter', ['ionic'])
         templateUrl: 'partials/search.html'
       }
     }
-    
+
   })
   .state('about', {
     url: '/about',
