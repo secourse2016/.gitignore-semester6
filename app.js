e49901182cb693d0935d3e2ba144601fac160d22
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-04-28 23:55:40 +0200

    removed edits made to control access

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.route('/error').get(sendIndex);
  */
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', ['X-Requested-With', 'Content-Type']);
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
