b7b0f3f2948c509971c1900e9e2fae08ad321862
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-04-28 23:40:56 +0200

    fixed css styling of dates

--- app.js
+++ app.js
@@ -45,8 +45,7 @@ app.route('/error').get(sendIndex);
  */
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', ['X-Requested-With', 'Content-Type']);
   next();
 });
 
